9d2b603d00e664fff88277afb63be7289610a73e
Calaglin <yich.z879248@msa.hinet.net>
2025-02-03 02:02:28 +0800

    商品功能 - 新增商品

--- src/main/java/com/calaglin/springboot_mall/dao/ProductDao.java
+++ src/main/java/com/calaglin/springboot_mall/dao/ProductDao.java
@@ -1,7 +1,9 @@
 package com.calaglin.springboot_mall.dao;
 
+import com.calaglin.springboot_mall.dto.ProductRequest;
 import com.calaglin.springboot_mall.model.Product;
 
 public interface ProductDao {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
--- src/main/java/com/calaglin/springboot_mall/service/ProductService.java
+++ src/main/java/com/calaglin/springboot_mall/service/ProductService.java
@@ -1,7 +1,9 @@
 package com.calaglin.springboot_mall.service;
 
+import com.calaglin.springboot_mall.dto.ProductRequest;
 import com.calaglin.springboot_mall.model.Product;
 
 public interface ProductService {
     Product getProductById(Integer productId);
+    Integer createProduct(ProductRequest productRequest);
 }
